405846cce85199a4455207fa015a0e92847311d1
baharaghayeva <baharragayeva@gmail.com>
2023-09-23 16:31:10 +0400

    final commit

--- assets/js/contentChange.js
+++ assets/js/contentChange.js
@@ -1,12 +1,24 @@
 function changeContent(contentId) {
     var contents = document.getElementsByClassName("product-section");
-    
+    var icons = document.querySelectorAll('.icons-container div');
+
     for (var i = 0; i < contents.length; i++) {
         contents[i].classList.remove("active");
     }
 
+    icons.forEach(x => x.classList.remove('active-btn'));
+
     var content = document.getElementById(contentId);
     if (content) {
         content.classList.add("active");
+    }
+
+    switch (contentId) {
+        case 'shop1':
+            icons[0].classList.add('active-btn');
+            break;
+        case 'shop2':
+            icons[1].classList.add('active-btn');
+            break;
     }
 }
